1ee8805681980e29d562d01c8c68ff45ed0fe9ef
ivan.hidayat <114371645+ivan-hidayat@users.noreply.github.com>
2026-06-11 16:57:51 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,43 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/4LynxX/Lynx/refs/heads/main/LynxxMain.lua"))()
+local VERSION = "1.0"
+local HUB_NAME = "Lynxx"
+local games = {
+    [6701277882]      = "https://raw.githubusercontent.com/4LynxX/all_Game/refs/heads/main/Fish_It.lua",
+    [9691752199]      = "https://raw.githubusercontent.com/4LynxX/all_Game/refs/heads/main/Sawah_Indo.lua",
+    [7326934954]      = "https://raw.githubusercontent.com/4LynxX/all_Game/refs/heads/main/99_nitf.lua",
+    [8316902627]      = "https://raw.githubusercontent.com/4LynxX/all_Game/refs/heads/main/pvb.lua",
+    [9721900284]      = "https://raw.githubusercontent.com/4LynxX/all_Game/refs/heads/main/fishzar.lua",
+    [9546331833]      = "https://raw.githubusercontent.com/4LynxX/all_Game/refs/heads/main/SambungKata.lua",
+    [6739698191]      = "https://raw.githubusercontent.com/4LynxX/all_Game/refs/heads/main/vd.lua",
+    [9465913467]      = "https://raw.githubusercontent.com/4LynxX/all_Game/refs/heads/main/is.lua",
+    [994732206]       = "https://raw.githubusercontent.com/4LynxX/all_Game/refs/heads/main/BloxFruit.lua",
+    [9186719164]       = "https://raw.githubusercontent.com/4LynxX/all_Game/refs/heads/main/sp.lua"
+}
+
+local universeId = game.GameId
+local placeId    = game.PlaceId
+local scriptURL  = games[universeId]
+
+print(string.format("[%s v%s] PlaceId: %d | UniverseId: %d", HUB_NAME, VERSION, placeId, universeId))
+
+if scriptURL then
+    print(string.format("[%s] Game supported! UniverseId: %d", HUB_NAME, universeId))
+    print(string.format("[%s] Loading script...", HUB_NAME))
+
+    local ok, err = pcall(function()
+        loadstring(game:HttpGet(scriptURL))()
+    end)
+
+    if not ok then
+        warn(string.format("[%s] Gagal load script: %s", HUB_NAME, tostring(err)))
+    end
+else
+    local msg = string.format(
+        "\n[%s] Game belum didukung!\nPlaceId: %d\nUniverseId: %d!",
+        HUB_NAME, placeId, universeId
+    )
+    warn(msg)
+    print(msg)
+end
 
 local UserGameSettings = UserSettings():GetService("UserGameSettings")
 local Lighting = game:GetService("Lighting")
